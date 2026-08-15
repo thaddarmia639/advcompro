ea4f8f601729816cc719c99489e04129f1be1236
Alexa <68012019@kmitl.ac.th>
2026-08-15 13:53:46 +0700

    done

---
+++ midterm/wk4/calculator.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    print(x+y)
+def subtract(x,y):
+    print(x - y)
+def multiply(x,y):
+    print(x * y)
+def divide(x,y):
+    print(x / y)
+
+add(5,6)
+subtract(10,6)
+multiply(10,6)
+divide(10,6)
